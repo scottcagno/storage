c9b3e35856e3b442c18233db7a6b75ffe077a6e2
Scott Cagno <scottiecagno@gmail.com>
2021-09-06 16:42:04 -0400

    Create hide_windows.go

---
+++ pkg/common/hide_windows.go
@@ -0,0 +1,19 @@
+// +build windows
+
+package common
+
+import (
+    "syscall"
+)
+
+func HideFile(filename string) error {
+    filenameW, err := syscall.UTF16PtrFromString(filename)
+    if err != nil {
+        return err
+    }
+    err = syscall.SetFileAttributes(filenameW, syscall.FILE_ATTRIBUTE_HIDDEN)
+    if err != nil {
+        return err
+    }
+    return nil
+}
