cb6e233afe89e603b783cd35435be2a661eda8df
scottcagno <scottiecagno@gmail.com>
2021-10-23 00:45:29 -0400

    [lsmt] facepalm.

--- pkg/lsmt/memtable/_v2/errors.go
+++
@@ -1,9 +0,0 @@
-package v2
-
-import "errors"
-
-var (
-	ErrFlushThreshold = errors.New("memtable: flush threshold has been reached")
-	ErrKeyNotFound    = errors.New("memtable: key not found")
-	ErrFoundTombstone = errors.New("memtable: found tombstone; entry was deleted")
-)
