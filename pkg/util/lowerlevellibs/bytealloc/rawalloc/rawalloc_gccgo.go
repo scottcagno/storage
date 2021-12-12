8a6bc7535003620ca756d3e5f0d91443f82eaee8
scottcagno <scottiecagno@gmail.com>
2021-12-12 13:15:45 -0500

    i really need to clean this up

--- pkg/util/lowerlevellibs/bytealloc/rawalloc/rawalloc_gccgo.go
+++ pkg/util/lowerlevellibs/bytealloc/rawalloc/rawalloc_gccgo.go
@@ -6,4 +6,7 @@ package rawalloc
 import "unsafe"
 
 //extern runtime.mallocgc
+//func mallocgc(size uintptr, typ unsafe.Pointer, needzero bool) unsafe.Pointer
+
+//go:linkname mallocgc runtime.mallocgc
 func mallocgc(size uintptr, typ unsafe.Pointer, needzero bool) unsafe.Pointer
